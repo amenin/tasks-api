14e5a7a622400513276fc5f02d1dfc8b926bdc18
Aline Menin <alinemenin@Alines-MacBook-Pro.local>
2024-05-02 10:52:34 +0200

    https server

--- api.js
+++ api.js
@@ -53,7 +53,20 @@ app.delete('/tasks/:id', (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Serveur en cours d'exécution sur le port ${port}`);
-});
+const port = 8024
+const portHTTPS = 8026
+
+app.listen(port, async () => { console.log(`HTTP Server started at port ${port}.`) })
+
+try {
+    var privateKey = fs.readFileSync( '/etc/httpd/certificate/exp_20240906/dataviz_i3s_unice_fr.key' );
+    var certificate = fs.readFileSync( '/etc/httpd/certificate/exp_20240906/dataviz_i3s_unice_fr_cert.crt' );
+    var ca = fs.readFileSync( '/etc/httpd/certificate/exp_20240906/dataviz_i3s_unice_fr_AC.cer' );
+    var options = {key: privateKey, cert: certificate, ca: ca};
+    https.createServer( options, function(req,res)
+    {
+        app.handle( req, res );
+    } ).listen( portHTTPS, async () => { console.log(`HTTPS Server started at port ${portHTTPS}.`) } );
+} catch(e) {
+    console.log("Could not start HTTPS server")
+}
